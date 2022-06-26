9eee51bc40fe8c2444258b83bc46a64b6e12ba81
Linda Rieksta <rieksta.linda20@gmail.com>
2022-06-26 18:44:09 +0300

    Added to Users model authentification

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -2,16 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use App\Models\Groups;
 use App\Models\Comments;
+use App\Models\Groups;
 use App\Models\Requests;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Users extends Model
+
+class Users extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
     protected $fillable = ['name', 'surname','email', 'password'];
+    protected $hidden = ['password', 'remember_token'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     public function groups()
     {
         return $this->hasMany(Groups::class);
@@ -24,4 +40,13 @@ class Users extends Model
     { // FK relationship
         return $this->belongsTo(Requests::class);
     }
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
