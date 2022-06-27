31d4d1d00062fde64701f6652cc18618694212b8
Linda Rieksta <rieksta.linda20@gmail.com>
2022-06-27 19:16:06 +0300

    Added All Requests button in navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,8 +13,10 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Home') }}
                     </x-nav-link>
+                    <x-nav-link href="/request/">All requests</x-nav-link>
+                    <x-nav-link href="/users/">All Users</x-nav-link>
                 </div>
             </div>
 
